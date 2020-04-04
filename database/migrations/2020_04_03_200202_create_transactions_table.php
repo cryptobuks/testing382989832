bbb3a038b020b0003686cd34eb46146427aed7f4
Hendra Danu Laksana <hdl.project.co@gmail.com>
2020-04-05 06:14:30 +0700

    feat(db: migration): adding some migration for transaction feature

---
+++ database/migrations/2020_04_03_200202_create_transactions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('type_id')->references('id')->on('transaction_types');
+            $table->integer('sender_id')->nullable()->references('id')->on('users');
+            $table->integer('recipient_id')->nullable()->references('id')->on('users');
+            $table->bigInteger('amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
